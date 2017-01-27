bbf74ae0305a251d4fc846a188c759679fcafe8e
inhyechoi <inhyechoi0404@gmail.com>
2017-01-26 16:45:34 -0800

    edited header on project 2

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
-// $(function() {
-// $('.home').click(function() {
-//         $('.logo').animate({
-//         'marginTop' : '-=30px' 
-//         });
-//     });
 
-// });
+$(function() {
+
+    $('.select_dropdown').on('change', function(){
+        console.log('hope');
+            $('.header').toggleClass('header-up');
+            $('.logo').toggleClass('logo-sizings');
+
+            
+    var url = 'https://api.nytimes.com/svc/topstories/v2/home.json';
+    url += '?' + $.param({
+    'api-key': 'e90ac7969c2046b0a19c7811bc84d6ee'
+    });
+    $.getJSON(url)
+
+.done(function(data) {
+  console.log(data);
+
+   $.each(data.results, function (index, value) {
+
+   })
+})
+})
+});
+
