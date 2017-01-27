d7beb5969ceb3e3b5cb670ff0f635b6a4fb8c4a8
inhyechoi <inhyechoi0404@gmail.com>
2017-01-26 23:06:06 -0800

    completed project2. 20170126

--- js/main.js
+++ js/main.js
@@ -1,25 +1,38 @@
 
 $(function() {
 
-    $('.select_dropdown').on('change', function(){
-        console.log('hope');
-            $('.header').toggleClass('header-up');
-            $('.logo').toggleClass('logo-sizings');
+  $('.select_dropdown').on('change', function(){
+    console.log('hope');
+      $('.header').toggleClass('header-up');
+      $('.logo').toggleClass('logo-sizings');
 
-            
-    var url = 'https://api.nytimes.com/svc/topstories/v2/home.json';
-    url += '?' + $.param({
-    'api-key': 'e90ac7969c2046b0a19c7811bc84d6ee'
-    });
-    $.getJSON(url)
 
-.done(function(data) {
-  console.log(data);
+  var url = 'https://api.nytimes.com/svc/topstories/v2/home.json';
+      url += '?' + $.param({
+      'api-key': 'e90ac7969c2046b0a19c7811bc84d6ee'
+  });
 
-   $.each(data.results, function (index, value) {
+  $.getJSON(url)
 
-   })
-})
-})
+    .done(function(data) {
+    var aaa = data.results;
+    console.log(aaa);
+
+//   function abc(value) {
+//   return value.;
+// }
+
+// var filtered = aaa.filter(abc);
+// console.log(abc(aaa[0]));
+// filtered is [12, 130, 44]
+
+
+//       $.each(data.results, function (index, value) {
+
+// console.log();     
+
+//       })
+    })
+  })
 });
 
