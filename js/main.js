8f33b2e63dee8454880dbc15178633910e87c417
inhyechoi <inhyechoi0404@gmail.com>
2017-03-08 09:55:52 -0800

    cleaning unnecessary code

--- js/main.js
+++ js/main.js
@@ -4,30 +4,28 @@ $(function () {
   $('.select_dropdown').on('change', function () {
     $('header').addClass('header_up').removeClass('header');
     $('.logo').addClass('logo_left').removeClass('.logo');
-    $('.logo_div').addClass('logo_div_sizing').removeClass('.logo_div');
+    $('.logo_conatiner').addClass('logo_container_sizing').removeClass('.logo_container');
     $('.selection_box').addClass('selection_box_sizing').removeClass('.selection_box');
 
     $('.loader').show();
 
-    var input = this.value;
-    var $contentList = $('.contentList');
-    $contentList.empty();
-    var url = 'https://api.nytimes.com/svc/topstories/v2/' + input + '.json';
-    url += '?' + $.param({
-      'api-key': 'e90ac7969c2046b0a19c7811bc84d6ee'
-    });
+    var input = $('select').val();
+    $('.contentList').empty();
+    var url = 'https://api.nytimes.com/svc/topstories/v2/' + input + '.json?api-key=e90ac7969c2046b0a19c7811bc84d6ee';
 
-    $.getJSON(url)
+   $.ajax({
+          url: url,
+          method: 'GET',
+          dataType: 'json'
+        })
       .done(function (data) {
-        $('.loader').hide();
         var dataList = data.results.filter(function (result) {
-          return result.multimedia.length;
+          return result.multimedia.length > 4;
+   
         }).splice(0, 12);
-
         var $viewImage = $('.contentList')
         var listItem = '';
-
-        $.each(dataList, function (data, value) {
+        $.each(dataList, function (index, value) {
           var title = value.abstract;
           var image = value.multimedia[4].url;
 
@@ -39,11 +37,7 @@ $(function () {
           listItem += '"/></a></li>'
         })
         $viewImage.append(listItem);
+        $('.loader').hide();
       })
-
-    $('.li-article').on('click', 'li', function (event) {
-      event.preventDefault();
-      window.open($(this).find('a').attr('href'));
-    });
   })
 });
